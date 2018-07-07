87edfba4b9e6dd022d48992b84f7e9c3c6f3245f
bikramjitsingh91 <bikram.jit.singh@outlook.com>
2018-07-06 20:39:25 -0400

    Calling the google Map Api to find the location

--- BillDueNotificationApp/app/src/main/java/com/example/bsvic/billduenotificationapp/MainActivity.java
+++ BillDueNotificationApp/app/src/main/java/com/example/bsvic/billduenotificationapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bsvic.billduenotificationapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button find = (Button) findViewById(R.id.button_id);
+        find.setOnClickListener((v)-> {
+            EditText postalCode = findViewById(R.id.postal_code);
+            String postalCodeText = postalCode.getText().toString();
+
+            //Getting the Google map
+            String address = "geo:0,0?q=" + postalCodeText;
+            Uri gmmIntentUri = Uri.parse(address);
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+
+        });
     }
 }
