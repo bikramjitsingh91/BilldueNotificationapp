261f08bb6a61973baad3a7baa533fe9833cda14c
bikramjitsingh91 <bikram.jit.singh@outlook.com>
2018-07-07 20:27:20 -0400

    added scan bill botton on view

--- BillDueNotificationApp/app/src/main/java/com/example/bsvic/billduenotificationapp/MainActivity.java
+++ BillDueNotificationApp/app/src/main/java/com/example/bsvic/billduenotificationapp/MainActivity.java
@@ -25,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
             mapIntent.setPackage("com.google.android.apps.maps");
             startActivity(mapIntent);
-
         });
+
+        //Button scanner = (Button) findViewById(R.id);
     }
 }
